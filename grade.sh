04b3eef682de722fb6545daa73e3bf5ca22c5c66
fallakm <161776457+fallakm@users.noreply.github.com>
2024-05-28 12:03:40 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,36 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# check if file is right 
+if [ -f "student-submission/ListExamples.java" ]; then
+    echo 'This is the correct file' 
+else
+    echo 'This is not the right type of file! Please submit again'
+    exit 1
+fi
+
+# copy the files over to grading-area
+cp TestListExamples.java student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+
+# switch over and compile 
+cd grading-area
+javac -cp $CPATH *.java 
+echo "The exit code for the compile step is $?."
+
+# check for failures 
+if [ $? -eq 0 ]; then 
+    echo 'All tests passed'
+else 
+    echo 'Some tests failed!'
+    echo 'Check above to find out why'
+fi
+java -cp $CPATH org.junit.runner.JUnitCore grading-area/TestListExamples | grep "Tests run: " > junit-output.txt
+
+FAILS=$(cat junit-output.txt | cut -d':' -f 3 | cut -d' ' -f 2)
+TOTAL=$(cat junit-output.txt | cut -d':' -f 2 | cut -d',' -f 1| cut -d' ' -f 2)
+
+echo "Fails: $FAILS"
+echo "Total: $TOTAL"
+echo "Numbers failed: $FAILS/$TOTAL" 
